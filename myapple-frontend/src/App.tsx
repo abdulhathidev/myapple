9cadbf32f7456dce47422454ec98f82aa1415d25
Abdul Hathi Dev <ayshaji2010@gmail.com>
2024-04-04 00:43:43 -0700

    Added gh-peges

--- myapple-frontend/src/App.tsx
+++ myapple-frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React JS application
+          Changes from npm to yarn
         </p>
         <a
           className="App-link"
