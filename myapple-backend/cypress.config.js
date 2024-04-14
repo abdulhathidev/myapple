962f09dd7f0c2f50c2e0e74097da1b773dde4834
Abdul Hathi Mohamed Hussain <abdulhathi@gmail.com>
2024-04-13 17:29:56 -0700

    port 4200

--- myapple-backend/cypress.config.js
+++ myapple-backend/cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/**/*.{feature,cy.js,js}',
     baseUrl: 'http://localhost:3001',
-    myPaymentsUrl: 'http://localhost:8081',
+    myPaymentsUrl: 'http://localhost:4200',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
